6f7781c74a48980ac69d001f017c5780237a36fa
Isao Yagi <isao@yahoo-inc.com>
2013-04-17 11:32:22 -0700

    ENOENT test

--- tests/create.js
+++ tests/create.js
@@ -11,14 +11,35 @@ test('[side-effects] create from fixture', function(t) {
         scan;
 
     t.plan(3);
-    
+
     function cb(err, msg) {
         t.false(err, 'null error');
         t.equal(+msg, 6, 'fs node count');
     }
-    
+
     scan = fn(from, to, {}, cb);
     scan.on('ignored', function(err, pathname) {
-    	t.equal(pathname.slice(-12), '.placeholder', '.placeholder ignored');
+        t.equal(pathname.slice(-12), '.placeholder', '.placeholder ignored');
+    });
+});
+
+test('create nonesuch', function(t) {
+    var from = 'oh hey, no. sorry.',
+        to = resolve(__dirname, 'artifacts'),
+        scan;
+
+    t.plan(6);
+
+    function cb(err, msg) {
+        t.false(err, 'null error');
+    }
+
+    scan = fn(from, to, {}, cb);
+    scan.on('error', function(err, pathname, stat) {
+        t.equal(pathname, from);
+        t.equal(err.code, 'ENOENT');
+        t.equal(err.errno, 34);
+        t.equal(pathname, err.path);
+        t.false(stat);
     });
 });
