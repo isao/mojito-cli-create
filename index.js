020530a8f4ad0e7cfb26704898435aaccd6bc7b7
Isao Yagi <isao@yahoo-inc.com>
2013-04-18 12:31:52 -0700

    fix getSourceDir bug after rename/mv, misc

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var path = require('path'),
 
 function error(msg, exit_code) {
     var err = new Error(msg);
-    err.errno = exit_code || 1;
+    err.errno = exit_code;
     return err;
 }
 
@@ -59,25 +59,25 @@ function getSourceDir(type, args) {
         err = type + ' is not a valid archetype or path.';
     }
 
-    return err ? errorWithUsage(err, 5) : source;
+    return source || errorWithUsage(err, 5);
 }
 
-function checkArgs(type, args) {
+function amMissingArgs(type, args) {
     var err;
 
     if (!type) {
         err = 'No parameters.';
 
     } else if (!args.length) {
-        err = 'Missing type, subtype, or source.';
+        err = 'Missing subtype, name or path.';
     }
 
-    return errorWithUsage(err, 3);
+    return err && errorWithUsage(err, 3);
 }
 
 function main(args, opts, meta, cb) {
     var type = args.shift() || '',
-        source = checkArgs(type, args) || getSourceDir(type, args),
+        source = amMissingArgs(type, args) || getSourceDir(type, args),
         name = args.shift(),
         keyval = util.parseCsvObj(opts.keyval),
         destination;
@@ -92,6 +92,7 @@ function main(args, opts, meta, cb) {
         keyval.name = name;
         keyval.port = opts.port || 8666;
         destination = path.join(opts.directory || '.', name);
+
         create(source, destination, keyval, cb);
     }
 }
@@ -124,6 +125,7 @@ exports.options = [
 ];
 
 exports.test = {
-    checkArgs: checkArgs,
     getSourceDir: getSourceDir
 };
+
+module.exports = exports;
