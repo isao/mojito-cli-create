aa24de741573b1bbbca606be25ed6ebd3a63cf21
Isao Yagi <isao@yahoo-inc.com>
2013-04-17 17:27:38 -0700

    break up main() into saner parts

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var path = require('path'),
 
 function error(msg, exit_code) {
     var err = new Error(msg);
-    err.code = exit_code || 1;
+    err.errno = exit_code || 1;
     return err;
 }
 
@@ -35,21 +35,8 @@ function subtypePath(type, args) {
     return path.join(type, subtype).toLowerCase(); // i.e. 'app/full'
 }
 
-function main(args, opts, meta, cb) {
-    var type = args.shift() || '',
-        source,
-        errmsg,
-        data;
-
-    if (!type) {
-        cb(errorWithUsage('Missing parameters. Please specify a type & name.'));
-        return source;
-    }
-
-    if (!args.length) {
-        cb(errorWithUsage('Missing parameter(s).', 3));
-        return source;
-    }
+function getSourceDir(type, args) {
+    var source, err;
 
     // get archetype source directory based on first one OR two arguments
     switch (type.toLowerCase()) {
@@ -57,45 +44,61 @@ function main(args, opts, meta, cb) {
     case 'mojit':
         // 1. mojito create [options] <app|mojit> [full|simple|default] <name>
         source = pathify(subtypePath(type, args));
-        errmsg = 'Invalid subtype.';
+        err = 'Invalid subtype.';
         break;
 
     case 'custom':
         // 2. mojito create [options] custom <path/to/archetype> <name>
         source = pathify(args.shift());
-        errmsg = 'Custom archtype path is invalid.';
+        err = 'Custom archtype path is invalid.';
         break;
 
     default:
         // 3. mojito create [options] <path/to/archetype> <name>
         source = pathify(type);
-        errmsg = type + ' is not a valid archetype or path.';
+        err = type + ' is not a valid archetype or path.';
     }
 
-    if (source) {
-        data = util.parseCsvObj(opts.keyval);
-        data.name = args.shift();
-        data.port = opts.port || 8666;
-
-        if (!data.name) {
-            cb(errorWithUsage('Missing name.', 3));
-            return source;
-        }
-
-        // ok, go.
-        create(source, path.join(opts.directory || '.', data.name), data, cb);
-
-    } else {
-        cb(errorWithUsage(errmsg, 5));
-    }
-
-    return source;
+    return err ? errorWithUsage(err, 5) : source;
 }
 
+function checkArgs(type, args) {
+    var err;
 
-module.exports = main;
+    if (!type) {
+        err = 'No parameters.';
 
-module.exports.usage = [
+    } else if (!args.length) {
+        err = 'Missing type, subtype, or source.';
+    }
+
+    return errorWithUsage(err, 3);
+}
+
+function main(args, opts, meta, cb) {
+    var type = args.shift() || '',
+        source = checkArgs(type, args) || getSourceDir(type, args),
+        name = args.shift(),
+        keyval = util.parseCsvObj(opts.keyval),
+        destination;
+
+    if (source instanceof Error) {
+        cb(source);
+
+    } else if (!name) {
+        cb(errorWithUsage('Missing name.', 3));
+
+    } else {
+        keyval.name = name;
+        keyval.port = opts.port || 8666;
+        destination = path.join(opts.directory || '.', name);
+        create(source, destination, keyval, cb);
+    }
+}
+
+exports = main;
+
+exports.usage = [
     'Usage: mojito create [options] <app|mojit> [full|simple|default] <name>',
     'Usage: mojito create [options] custom <path/to/archetype> <name>',
     'Usage: mojito create [options] <path/to/archetype> <name>',
@@ -114,10 +117,13 @@ module.exports.usage = [
     '                   commas: "key1:val1,key2:val2',
     '  -k [string]      Short for --keyval'].join('\n');
 
-module.exports.options = [
+exports.options = [
     {shortName: 'd', hasValue: true,  longName: 'directory'},
     {shortName: 'k', hasValue: true,  longName: 'keyval'},
     {shortName: 'p', hasValue: true,  longName: 'port'}
 ];
 
-
+exports.test = {
+    checkArgs: checkArgs,
+    getSourceDir: getSourceDir
+};
