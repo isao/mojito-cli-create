98560943223330cc619bd6564938966796f93a25
Isao Yagi <isao@yahoo-inc.com>
2013-04-23 15:26:01 -0700

    rm unused

--- conf/defaults.js
+++
@@ -1,8 +0,0 @@
-var resolve = require('path').resolve;
-
-module.exports = {
-    'clobber': false,
-    'remove': false,
-    'port': 8666,
-    'sources': ['.', resolve(__dirname, '../archetypes')]
-};
